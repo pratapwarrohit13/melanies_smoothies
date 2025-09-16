1c19ac545ccd21b7d5464a7c71192ed0207d2596
Rohit Pratapwar <pratapwarrohit13@live.com>
2025-09-16 13:39:32 +0530

    Update streamlit_app.py
    adding iloc

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col,when_matched
 import requests
 
@@ -22,7 +23,9 @@ name_on_order = st.text_input ('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
+pd_df = my_dataframe.to_pandas()
 # st.dataframe(data=my_dataframe, use_container_width=True)
 ingradients_list = st.multiselect('Choose any 5 items',my_dataframe,max_selections=5)
 
@@ -34,6 +37,10 @@ if ingradients_list:
     for fruit_chosen in ingradients_list:
       
       ingradients_string += fruit_chosen + ' '
+
+      search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
       st.subheader(fruit_chosen + 'Nutrition Information')
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
       sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)     
